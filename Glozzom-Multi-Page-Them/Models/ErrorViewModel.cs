0fbe237ace2f70f69c6d3ee9b6087b77c9f680d5
Sachin Limbu <sachinlimbu@outlook.com>
2019-08-05 01:01:44 +0100

    Add project files.

---
+++ Glozzom-Multi-Page-Them/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Glozzom_Multi_Page_Them.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
